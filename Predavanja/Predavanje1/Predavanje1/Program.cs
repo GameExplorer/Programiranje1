c83d01614d92495e9232d35961cb6cd78d01ce3f
GameExplorer <GameExplorer@users.noreply.github.com>
2024-01-11 20:37:44 +0100

    Predavanje 1

---
+++ Predavanja/Predavanje1/Predavanje1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Predavanje1
+{
+    class Predavanje1
+    {
+        static void Main(String[] args)
+        {
+            
+        }
+    }
+}
