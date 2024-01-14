6d5143f907e4e71c7d18065922ed249009411802
GameExplorer <GameExplorer@users.noreply.github.com>
2024-01-14 17:22:17 +0100

    misc

--- Predavanja/Predavanje1/Predavanje1/obj/Debug/net8.0/Predavanje1.AssemblyInfo.cs
+++ Predavanja/Predavanje1/Predavanje1/obj/Debug/net8.0/Predavanje1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Predavanje1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a59179c1eb6d6071ff75fdd6ee866b8886c9f799")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Predavanje1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Predavanje1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
