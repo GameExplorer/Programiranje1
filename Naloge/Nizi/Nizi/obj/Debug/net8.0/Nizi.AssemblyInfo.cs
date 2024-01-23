69232c4d0a56846194d53f28da35873418f69fed
GameExplorer <petar.petrovic2001@gmail.com>
2024-01-23 20:55:02 +0100

    misc

--- Naloge/Nizi/Nizi/obj/Debug/net8.0/Nizi.AssemblyInfo.cs
+++ Naloge/Nizi/Nizi/obj/Debug/net8.0/Nizi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nizi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b8cde431e0297d7b678b6f4333ab792b1a2755")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nizi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nizi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
