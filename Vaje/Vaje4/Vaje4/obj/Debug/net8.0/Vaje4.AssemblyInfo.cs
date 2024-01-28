9d35c0a0170d43897f92bda17304fff631d94579
GameExplorer <petar.petrovic2001@gmail.com>
2024-01-28 21:06:39 +0100

    misc

--- Vaje/Vaje4/Vaje4/obj/Debug/net8.0/Vaje4.AssemblyInfo.cs
+++ Vaje/Vaje4/Vaje4/obj/Debug/net8.0/Vaje4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vaje4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93068fbfc11e6982c957e384769081a9a9863d28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vaje4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vaje4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
