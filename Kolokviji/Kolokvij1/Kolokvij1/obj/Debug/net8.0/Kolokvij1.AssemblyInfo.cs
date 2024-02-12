af00953b060180f39858ac8de5e7f723feee4b59
GameExplorer <GameExplorer@users.noreply.github.com>
2024-02-12 20:00:05 +0100

    2020

--- Kolokviji/Kolokvij1/Kolokvij1/obj/Debug/net8.0/Kolokvij1.AssemblyInfo.cs
+++ Kolokviji/Kolokvij1/Kolokvij1/obj/Debug/net8.0/Kolokvij1.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kolokvij1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db7feade81b1d55a6e100d62100415901dba7b09")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c3fa8179e1952dad45a41295ba4bfc183f1e1ad")]
->>>>>>> Stashed changes
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kolokvij1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kolokvij1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
