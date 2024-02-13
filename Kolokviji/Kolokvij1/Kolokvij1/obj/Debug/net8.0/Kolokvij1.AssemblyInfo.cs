86fe10211329d804a54ffadcbbcbd45c61c2ec42
GameExplorer <GameExplorer@users.noreply.github.com>
2024-02-13 13:24:25 +0100

    Kolokviji 2020

--- Kolokviji/Kolokvij1/Kolokvij1/obj/Debug/net8.0/Kolokvij1.AssemblyInfo.cs
+++ Kolokviji/Kolokvij1/Kolokvij1/obj/Debug/net8.0/Kolokvij1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kolokvij1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0637b95348adbb3b62b5f8caf8a9dc15f0566b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kolokvij1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kolokvij1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
