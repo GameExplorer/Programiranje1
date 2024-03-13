62a59037dc7185812aa3e73c721c87e420f1daaf
GameExplorer <GameExplorer@users.noreply.github.com>
2024-03-13 18:40:43 +0100

    Datoteke

---
+++ Naloge/Datoteke/RazredFileOsnove/Vaje10/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Vaje10
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            //1. Naloga
+            
+            
+            //2. Naloga
+            string trenutniDirektorij = Directory.GetCurrentDirectory();
+            string[] datoteke = Directory.GetFiles(trenutniDirektorij);
+
+            foreach (string dat in datoteke)
+            {
+                Console.WriteLine(dat + " " + Directory.GetCreationTime(dat));
+            }
+
+        }
+    }
+}
