308c90e8051d9f668cd94ed464e43bbafd35e08e
GameExplorer <petar.petrovic2001@gmail.com>
2024-03-14 18:11:07 +0100

    Vaje10

--- Vaje/Vaje10/Vaje10/obj/Debug/net8.0/Vaje10.AssemblyInfo.cs
+++ Vaje/Vaje10/Vaje10/obj/Debug/net8.0/Vaje10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vaje10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cdae0af8f9173fc2ba9ed78bb5dd5684e24c96b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+021eb06c838a43ab681f8cde7d24f8ab01797745")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vaje10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vaje10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
