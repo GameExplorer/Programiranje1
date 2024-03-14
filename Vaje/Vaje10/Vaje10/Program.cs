5cdae0af8f9173fc2ba9ed78bb5dd5684e24c96b
GameExplorer <GameExplorer@users.noreply.github.com>
2024-03-14 13:56:20 +0100

    Vaje 10

--- Vaje/Vaje10/Vaje10/Program.cs
+++ Vaje/Vaje10/Vaje10/Program.cs
@@ -20,14 +20,44 @@ namespace Vaje10
 
     class AvtoDatoteka
     {
-        
+        private string imeDatoteke;
+
+        public AvtoDatoteka(string imeDatoteke)
+        {
+            this.imeDatoteke = imeDatoteke;
+        }
+
+        public void ShranjevanjeVozil(Avto[] tabelaAvtomobilov)
+        {
+            StreamWriter sw = new StreamWriter(imeDatoteke);
+
+            foreach (Avto avto in tabelaAvtomobilov)
+            {
+                sw.Write($"{avto.Znamka};{avto.Tip};{avto.Letnik};{avto.stPrevozenihKilometrov}");
+            }
+        }
+
+        public Avto[] BranjePodatkov()
+        {
+            string[] vrstice = File.ReadAllLines(imeDatoteke);
+            Avto[] tabelaAvtomobilov = new Avto[vrstice.Length];
+
+            for (int i = 0; i < vrstice.Length; i++)
+            {
+                string[] podatki = vrstice[i].Split(";");
+                tabelaAvtomobilov[i] =
+                    new Avto(podatki[0], podatki[1], Int32.Parse(podatki[2]), Double.Parse(podatki[3]));
+            }
+
+            return tabelaAvtomobilov;
+        }
     }
     class Program
     {
         public static void Main(String[] args)
         {
             //1. Naloga
-            args = new[] { "" };
+            args = new[] { "NarediMapo" };
             string mapa = args[0];
 
             if (mapa.Length > 0  && !mapa.Contains('#') && !mapa.Contains('&') &&
@@ -63,7 +93,103 @@ namespace Vaje10
             }
 
             //3. Naloga
-            
+            char izbira;
+            string imeDatoteke = ""; //začetno ime datoteke
+            Avto[] tabelaAvtomobilov = null; //na začetku je tabela avtomobilov prazna
+            AvtoDatoteka datotekaAvtomobilov = null; //datoteka avtomobilov na začetku prazna
+
+            while (true)
+            {
+                Console.WriteLine("SEZNAM VOZIL");
+                Console.WriteLine("-----------------------------------------------------------------------");
+                Console.WriteLine("1. Vpiši ime datoteke");
+                Console.WriteLine("2. Priprava testnih podatkov");
+                Console.WriteLine("3. Izpis seznama vozil");
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("4. Shranjevanje vozil v datoteko");
+                Console.WriteLine("5. Branje podatkov iz datoteke");
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("X. Izhod Programa");
+                
+                izbira = Console.ReadLine()[0];
+
+                switch (izbira)
+                {
+                    case '1':
+                        Console.Write("Vnesi ime datoteke: ");
+                        string vnos = Console.ReadLine();
+                        datotekaAvtomobilov = new AvtoDatoteka(vnos);
+                        break;
+                    case '2':
+                        tabelaAvtomobilov = TestniPodatki();
+                        break;
+                    case '3':
+                        IzpisSeznamVozil(tabelaAvtomobilov);
+                        break;
+                    case '4':
+                        if (tabelaAvtomobilov != null && datotekaAvtomobilov != null)
+                        {
+                            datotekaAvtomobilov.ShranjevanjeVozil(tabelaAvtomobilov);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Vnesi ime datoteke in napolni in nato napolni datoteko");
+                        }
+                        break;
+                    case '5':
+                        if (tabelaAvtomobilov != null)
+                        {
+                            tabelaAvtomobilov = datotekaAvtomobilov.BranjePodatkov();
+                            Console.WriteLine("Podatki uspešno prebrani");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Napaka pri branju podatkov.");
+                        }
+                        break;
+                    case 'X':
+                    case 'x':
+                        Console.WriteLine("Izhod iz programa");
+                        return;
+                    default:
+                        Console.WriteLine("Napačna izbira!");
+                        break;
+                }
+            }
+        }
+
+        public static Avto[] TestniPodatki()
+        {
+            Avto[] tabelaAvtomobilov = new Avto[10];
+
+            tabelaAvtomobilov[0] = new Avto("Nissan", "Skyline Type GT-R R34", 1999, 7000);
+            tabelaAvtomobilov[1] = new Avto("Ferrari", "F40", 1987, 1245);
+            tabelaAvtomobilov[2] = new Avto("Mclaren", "F1", 1992, 63333);
+            tabelaAvtomobilov[3] = new Avto("Toyota", "Supra", 1993, 100000);
+            tabelaAvtomobilov[4] = new Avto("Acura", "Integra Type R", 1997, 52001);
+            tabelaAvtomobilov[5] = new Avto("Porsche", "Cayman", 2005, 89642);
+            tabelaAvtomobilov[6] = new Avto("Subaru", "WRX", 2002, 164210);
+            tabelaAvtomobilov[7] = new Avto("Bugatti", "Veyron", 2005, 176424);
+            tabelaAvtomobilov[8] = new Avto("Ford", "GT", 2005, 30512);
+            tabelaAvtomobilov[9] = new Avto("Porsche", "918 Spyder", 2013, 200514);
+
+            return tabelaAvtomobilov;
+        }
+
+        public static void IzpisSeznamVozil(Avto[] tabelaAvtomobilov)
+        {
+            if (tabelaAvtomobilov != null)
+            {
+                Console.WriteLine("Seznam vozil: ");
+                foreach (Avto avtomobil in tabelaAvtomobilov)
+                {
+                    Console.WriteLine("Znamka: {0}, Tip: {1}, Letnik: {2}", avtomobil.Znamka, avtomobil.Tip, avtomobil.Letnik);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Tabela vozil je prazna");
+            }
         }
     }
 }
